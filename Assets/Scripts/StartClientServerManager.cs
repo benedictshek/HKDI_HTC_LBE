866d7fd77105570c967f2ccff1a9251c896406a2
benedictshek <49088947+benedictshek@users.noreply.github.com>
2025-05-27 12:01:12 +0800

    Updated Client-server in SampleScene

---
+++ Assets/Scripts/StartClientServerManager.cs
@@ -0,0 +1,24 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class StartClientServerManager : MonoBehaviour
+{
+    private NetworkManager m_NetworkManager;
+
+    private void Awake()
+    {
+        m_NetworkManager = GetComponent<NetworkManager>();
+    }
+
+    private void Start()
+    {
+        if (Application.isEditor)
+        {
+            m_NetworkManager.StartServer();
+        }
+        else
+        {
+            m_NetworkManager.StartClient();
+        }
+    }
+}
