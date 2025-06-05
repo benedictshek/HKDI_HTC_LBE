554fc0d1062356787a8710a98661d2c499b0941a
benedictshek <49088947+benedictshek@users.noreply.github.com>
2025-06-05 17:27:15 +0800

    Added AdjustPlayerHeight when step on platform

--- Assets/Scripts/Player/PlayerCollision.cs
+++ Assets/Scripts/Player/PlayerCollision.cs
@@ -25,4 +25,27 @@ public class PlayerCollision : MonoBehaviour
         _playerCollider.height = height;
         transform.position = _playerHead.position - Vector3.up * height / 2;
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Lift"))
+        {
+            Debug.Log("Player stepped on the lift.");
+            AdjustPlayerHeight(other.transform.position.y);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Lift"))
+        {
+            Debug.Log("Player left the lift.");
+            AdjustPlayerHeight(0);
+        }
+    }
+
+    private void AdjustPlayerHeight(float stepHeight)
+    {
+        _xrOrigin.transform.position = new Vector3(_xrOrigin.transform.position.x, stepHeight, _xrOrigin.transform.position.z);
+    }
 }
