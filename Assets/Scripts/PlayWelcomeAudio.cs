3ba3f16bf2c68ff05d9b1b298bc2fef0f6d8553c
benedictshek <49088947+benedictshek@users.noreply.github.com>
2025-06-10 09:51:04 +0800

    Added more vocals

--- Assets/Scripts/PlayWelcomeAudio.cs
+++ Assets/Scripts/PlayWelcomeAudio.cs
@@ -3,7 +3,14 @@ using UnityEngine;
 public class PlayWelcomeAudio : MonoBehaviour
 {
     [SerializeField] private float waitTimeForDelay = 15f;
+    [SerializeField] private float waitTimeForDelay2 = 10f;
+    [SerializeField] private float waitTimeForDelay3 = 2f;
+    [SerializeField] private float waitTimeForDelay4 = 2f;
     private AudioSource audioSource;
+    public AudioClip audioClip;
+    public AudioClip audioClip2;
+    public AudioClip audioClip3;
+    public AudioClip audioClip4;
 
     void Start()
     {
@@ -20,11 +27,30 @@ public class PlayWelcomeAudio : MonoBehaviour
         // Play the audio clip
         if (audioSource != null)
         {
+            audioSource.clip = audioClip;
             audioSource.Play();
         }
         else
         {
             Debug.LogWarning("AudioSource component is missing!");
         }
+        
+        yield return new WaitForSeconds(audioClip.length);
+        yield return new WaitForSeconds(waitTimeForDelay2);
+
+        audioSource.clip = audioClip2;
+        audioSource.Play();
+        
+        yield return new WaitForSeconds(audioClip2.length);
+        yield return new WaitForSeconds(waitTimeForDelay3);
+        
+        audioSource.clip = audioClip3;
+        audioSource.Play();
+        
+        yield return new WaitForSeconds(audioClip3.length);
+        yield return new WaitForSeconds(waitTimeForDelay4);
+        
+        audioSource.clip = audioClip4;
+        audioSource.Play();
     }
 }
